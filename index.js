f0e65eaa290729cecf9ea03a5ebbf3481360320f
Dalip Kumar <dalipyadav68@gmail.com>
2025-07-15 22:42:15 +0530

    Fileanalyse Route added

--- index.js
+++ index.js
@@ -17,10 +17,6 @@ app.get('/', (req, res) => {
 
 // File upload route
 app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
-  if (!req.file) {
-    return res.status(400).json({ error: 'No file uploaded' });
-  }
-
   res.json({
     name: req.file.originalname,
     type: req.file.mimetype,
